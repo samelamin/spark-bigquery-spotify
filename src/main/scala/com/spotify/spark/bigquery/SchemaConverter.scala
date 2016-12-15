7d6d549e53dabe6e3cde37ee6d0ddaaf615f4a3f
sam.elamin <sam.elamin@just-eat.com>
2016-12-15 12:40:07 +0000

    pass failing test

--- src/main/scala/com/spotify/spark/bigquery/SchemaConverter.scala
+++ src/main/scala/com/spotify/spark/bigquery/SchemaConverter.scala
@@ -1,15 +1,31 @@
 package com.spotify.spark.bigquery
 
-import com.google.api.services.bigquery.model.TableSchema
+
+import com.google.api.services.bigquery.model.{TableFieldSchema, TableSchema}
+import org.apache.spark.sql.types._
+
+import scala.collection.JavaConverters._
 
 /**
   * Created by root on 14/12/2016.
   */
 object SchemaConverter {
 
-  def convertDataframeSchemaToTableSchema(schema: String): TableSchema = {
-    val tableSchema = new TableSchema()
+  def dfToBQSchema(schema: StructType): TableSchema = {
+    val schemaFields = schema.fields.map { field =>
+      val fieldType = field.dataType match {
+        case ByteType | ShortType | IntegerType | LongType => "INTEGER"
+        case FloatType | DoubleType => "FLOAT"
+        case _: DecimalType | StringType => "STRING"
+        case BinaryType => "BYTES"
+        case BooleanType => "BOOLEAN"
+        case TimestampType => "TIMESTAMP"
+        case ArrayType(_, _) | MapType(_, _, _) | _: StructType => "RECORD"
+      }
+      new TableFieldSchema().setName(field.name).setType(fieldType)
+    }.toList.asJava
 
+    val tableSchema = new TableSchema().setFields(schemaFields)
     tableSchema
   }
 
--- src/test/scala/DataframeConverterSpecs.scala
+++ src/test/scala/DataframeConverterSpecs.scala
@@ -3,51 +3,72 @@ import org.apache.spark
 import org.apache.spark.sql.{SQLContext, SparkSession}
 import org.scalatest.Matchers._
 import org.scalatest.{FeatureSpec, GivenWhenThen}
+import com.spotify.spark.bigquery._
+import org.apache.spark.sql.types.StructType
 
 class DataframeConverterSpecs extends FeatureSpec with GivenWhenThen with DataFrameSuiteBase {
-  feature("Schema Converters") {
-    scenario("When converting a dataframe schema to BQ TableSchema") {
-      Given("A dataframe schema")
-
+  feature("Schema Converters. Dataframe To BQ Schema") {
+    scenario("When converting a simple dataframe") {
+      Given("A dataframe")
       val sqlCtx = sqlContext
-
-      val sampleNestedJson = """{
+      val sampleJson = """{
                          |	"id": 1,
-                         |	"error": null,
-                         |	"result": {
-                         |		"nPeople": 2,
-                         |		"people": [{
-                         |			"namePeople": "Inca",
-                         |			"power": "1235",
-                         |			"location": "asdfghjja",
-                         |			"idPeople": 189,
-                         |			"mainItems": "brownGem",
-                         |			"verified": false,
-                         |			"description": "Lorem impsum bla bla",
-                         |			"linkAvatar": "avatar_12.jpg",
-                         |			"longitude": 16.2434263,
-                         |			"latitude": 89.355118
-                         |		}, {
-                         |			"namePeople": "Maya",
-                         |			"power": "1235",
-                         |			"location": "hcjkjhljhl",
-                         |			"idPeople": 119,
-                         |			"mainItems": "greenstone",
-                         |			"verified": false,
-                         |			"description": "Lorem impsum bla bla",
-                         |			"linkAvatar": "avatar_6.jpg",
-                         |			"longitude": 16.2434263,
-                         |			"latitude": 89.3551185
-                         |		}]
-                         |	}
+                         |	"error": null
                          |}""".stripMargin
+
+      val df = sqlContext.read.json(sc.parallelize(List(sampleJson)))
+      val dfSchema = df.schema
+
+      When("Passing the schema to the converter")
+      val tableSchema = SchemaConverter.dfToBQSchema(dfSchema)
+      Then("We should receive a BQ Table Schema")
+      tableSchema should not be null
+      tableSchema.getFields should not be null
+    }
+
+    scenario("When converting a complex dataframe with nested data") {
+      Given("A dataframe")
+      val sqlCtx = sqlContext
+      val sampleNestedJson = """{
+                               |	"id": 1,
+                               |	"error": null,
+                               |	"result": {
+                               |		"nPeople": 2,
+                               |		"people": [{
+                               |			"namePeople": "Inca",
+                               |			"power": "1235",
+                               |			"location": "asdfghjja",
+                               |			"idPeople": 189,
+                               |			"mainItems": "brownGem",
+                               |			"verified": false,
+                               |			"description": "Lorem impsum bla bla",
+                               |			"linkAvatar": "avatar_12.jpg",
+                               |			"longitude": 16.2434263,
+                               |			"latitude": 89.355118
+                               |		}, {
+                               |			"namePeople": "Maya",
+                               |			"power": "1235",
+                               |			"location": "hcjkjhljhl",
+                               |			"idPeople": 119,
+                               |			"mainItems": "greenstone",
+                               |			"verified": false,
+                               |			"description": "Lorem impsum bla bla",
+                               |			"linkAvatar": "avatar_6.jpg",
+                               |			"longitude": 16.2434263,
+                               |			"latitude": 89.3551185
+                               |		}]
+                               |	}
+                               |}""".stripMargin
+
       val df = sqlContext.read.json(sc.parallelize(List(sampleNestedJson)))
+      val dfSchema = df.schema
 
+      When("Passing the schema to the converter")
+      val tableSchema = SchemaConverter.dfToBQSchema(dfSchema)
 
-      print("*********** test me")
-      val count = df.count()
-      df.schema should not be null
-
+      Then("We should receive a BQ Table Schema")
+      tableSchema should not be null
+      tableSchema.getFields should not be null
     }
   }
 }
